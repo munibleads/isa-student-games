44c6865d9e3ed182a00f61836f100925c75a9a3e
Munib Leads <munibleads@gmail.com>
2025-05-07 20:55:46 +0300

    Remove unused collapsible component

--- src/components/ui/collapsible.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-import * as CollapsiblePrimitive from "@radix-ui/react-collapsible"
-
-const { Root, Trigger, Content } = CollapsiblePrimitive
-
-export const Collapsible = Root
-export const CollapsibleTrigger = Trigger
-export const CollapsibleContent = Content 
